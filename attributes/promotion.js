dbeb2cd85923243c20501d631250f84c441e865c
eirieiri <klaudia77@live.de>
2023-11-20 03:14:57 +0100

    update to promotion

--- attributes/promotion.js
+++ attributes/promotion.js
@@ -1,4 +1,6 @@
 const PromotionStatus = {
-    PREPROMOTE : true, //is a prepromote
-    NOPROMO : false //is not a prepromote 
+    PREPROMOTE : "prepromote",
+    VILLAGER : "villager",
+    STANDARD : "standard",
+    NOPROMOTION : "no promotion"
 }
